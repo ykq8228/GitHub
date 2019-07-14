3638f4e50f4601545617f2f3357b8b8053410b8d
ykq8228 <52562258+ykq8228@users.noreply.github.com>
2019-07-14 11:23:06 +0800

    Add files via upload

---
+++ Demo1.java
@@ -0,0 +1,18 @@
+//计算三十个人的平均分,使用数组实现
+
+import java.util.*;
+public class Demo1{
+	public static void main(String[] args){
+		Scanner input = new Scanner(System.in);
+		double[]scores = new double[5]; 
+		double sum = 0;
+		for(int i=0;i<scores.length;i++){
+			System.out.print("请输入第"+(i+1)+"位同学的成绩：");
+			//为数组里的30个格子赋值
+			scores [i] = input.nextDouble();
+			sum += scores[i];
+		}
+		double avg = sum/scores.length;
+		System.out.print("班级同学的平均分为："+avg);
+	}
+}
